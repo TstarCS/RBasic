5ae8c90a0af15aec863e6d294d80a6ee41904072
Erincatcat <38828784+Erincatcat@users.noreply.github.com>
2019-03-18 17:18:02 +0800

    lesson7

--- Lesson7/material.R
+++ Lesson7/material.R
@@ -5,6 +5,9 @@ setwd("C:/Users/ErinKuo/Desktop/r_class")
 library(ggplot2)
 library(tidyverse)
 library(gridExtra) #畫面切割
+library(ggcorrplot) #筆電不能做
+library(car)
+
 set.seed(100)
 df <- diamonds[sample(1:nrow(diamonds),5000),]
 #認識資料
@@ -52,7 +55,7 @@ ggplot(df, aes(cut, price)) +
 ggplot(df, aes(cut, price,fill=cut)) +  
   geom_boxplot()+
   labs(x = "切工", y = "價格", title = "盒鬚圖")   
-  
+
 #箱子的外框顏色與outlier顏色和圖形
 ggplot(df, aes(cut, price,fill=cut)) +
   geom_boxplot(colour = "#3366FF",
@@ -120,15 +123,15 @@ ggplot(df, aes(x = price, fill = cut)) +
 #   labs(x = "價格", y = "個數", title = "直方圖")
 
 # HW1 ----------------------------------------------------------------------
-
+library(tidyverse)
 member <- read.csv("C:/Users/ErinKuo/Desktop/r_class/HW/人員名單.csv",
                    col.names = c("dep","team","name","gender"))
-  #單位=dep ; 組別=team ; 人員=name ; 性別=gender
+#單位=dep ; 組別=team ; 人員=name ; 性別=gender
 BC10801 <- read.csv("C:/Users/ErinKuo/Desktop/r_class/HW/BC10801.csv",
                     col.names = c("name","billcycle","totaldebt","receive"))
 BC10802 <- read.csv("C:/Users/ErinKuo/Desktop/r_class/HW/BC10802.csv",
-              col.names = c("name","billcycle","totaldebt","receive")) 
-  #人員姓名=name ; 帳單週期=billcycle ; 委案金額=totaldebt ; 回收金額=receive
+                    col.names = c("name","billcycle","totaldebt","receive")) 
+#人員姓名=name ; 帳單週期=billcycle ; 委案金額=totaldebt ; 回收金額=receive
 #typo1-2月帳周出現1月帳周
 table(BC10802$billcycle)
 BC10802$billcycle[BC10802$billcycle=="2019/1/20"] <- "2019/2/20"
@@ -151,7 +154,7 @@ ggplot(df, aes(team, receive,fill = gender)) +
         axis.title.y = element_text(color='darkblue',face = "bold", angle=0, vjust = 0.5),
         title =  element_text(color="darkgreen",face = "bold")) + 
   scale_fill_discrete(name="性別")
-  
+
 
 # Day2 --------------------------------------------------------------------
 library(tidyverse)
@@ -168,7 +171,7 @@ ggplot(df, aes(carat, price)) +
 ggplot(df, aes(carat, price)) +  
   geom_point(stat = "identity",
              position = "identity") + 
-  stat_smooth(se = T)
+  stat_smooth(se = T) #解釋跳出的warning
 #線性
 ggplot(df, aes(carat, price)) +  
   geom_point(stat = "identity",
@@ -196,30 +199,38 @@ ggplot(df, aes(carat, price, colour = cut)) +  #這裡是資料集及軸及顏色設定
   theme(axis.title.x = element_text(color='darkgreen',face = "bold"),
         axis.title.y = element_text(color='darkgreen',face = "bold"))
 #筆電不能做#當變數很多，不想要一個一個慢慢畫時，可以一次看到多變項間彼此關係
+library(car)
 scatterplotMatrix(~carat+price+depth+table,
                   data=df,
-                  spread=FALSE,
-                  lty=2,
                   main="Scatter Plot Matrix")
-
 
 
 #Correlogram
 library(ggcorrplot) #筆電不能做
 df1 <- df[sapply(df, is.numeric)]
 corr <- round(cor(df1),1)
-pairs(~.,data=df,main="Matrix Scatter Plot")
 View(corr)
 #把相關係數畫成圖形
+library(car)
 ggcorrplot(corr, 
-           hc.order = F,  #是否把相關係數大小作排序
-           type = "lower", #full,upper,lower
-           lab = TRUE,  #要不要標示數字
-           lab_size = 3,  #字型大小
-           method="circle", #square
-           colors = c("red", "white", "lightgreen"), #漸層顏色
+           hc.order = F,  
+           type = "lower", 
+           lab = TRUE,  
+           lab_size = 3,  
+           method="circle", 
+           colors = c("red", "white", "lightgreen"), 
            title="Correlogram of diamonds", 
            ggtheme=theme_bw)
+#相同的,用來文字說明
+# ggcorrplot(corr, 
+#            hc.order = F,  #是否把相關係數大小作排序
+#            type = "lower", #full,upper,lower
+#            lab = TRUE,  #要不要標示數字
+#            lab_size = 3,  #字型大小
+#            method="circle", #square
+#            colors = c("red", "white", "lightgreen"), #漸層顏色
+#            title="Correlogram of diamonds", 
+#            ggtheme=theme_bw)
 
 #熱力圖----------------
 #用我們的df-類別
@@ -237,11 +248,13 @@ ggplot(heat, aes(cut, color)) +
 #   geom_tile(aes(fill = price)) + 
 #   theme_bw() + 
 #   scale_fill_gradient(low="white", high="red")
+
 #匯出圖形
+getwd()
 ggsave(plot, file="plot2.png")
 ggsave(plot, file="plot3.png", width=6, height=4, units = "in")
 
-
+###################
 #馬賽克
 #install.packages("vcd")
 library(vcd)
@@ -261,12 +274,45 @@ mosaic(Titanic,
 
 
 # HW2 ----------------------------------------------------------------------
+library(tidyverse)
+library(car)
+
+hw1 <- read.csv("//vibo/nfs/CHPublic/客戶服務事業部/部門公用區/R語言課程/HW2/課程內容及題目/Lottery.csv",fileEncoding = 'UTF-8') %>% 
+  select(ID,birthymd="出生日期",buyymd="購買日期",sex="性別",
+         pay="投注金額",get="中獎金額",area="購買地區",religion="宗教信仰")  
+#hw2 <- read.csv("//vibo/nfs/CHPublic/客戶服務事業部/部門公用區/R語言課程/HW7/課程內容及題目/df2.csv",fileEncoding = 'UTF-8')
+
+hw1_1 <- hw1 %>%  #將
+  mutate(birthymd=as.Date(birthymd),
+         age = as.integer(round((Sys.Date()-birthymd)/365,0)),
+         get=as.integer(get),
+         sex = case_when(
+           sex == "汝" ~ "女", 
+           sex == "女" ~ "女",
+           sex == "男" ~ "男"
+         ))
+
+set.seed(100)
+hw1_2 <- hw1_1[sample(1:nrow(hw1_1),3000),]
+
+scatterplotMatrix(~pay+get+age|sex,
+                  regLine=FALSE,
+                  data=hw1_2,
+                  spread=FALSE,
+                  lty=2,
+                  main="Scatter Plot Matrix",
+                  diagonal=list(method="boxplot"),
+                  pch=2:3, #pch=2三角形 =3+
+                  col= c("green","orange") #不能用default顏色
+                  )
+
+# Day 3 -------------------------------------------------------------------
 
 
 #善行圖
 ggplot(mpg)+geom_bar(width=1, aes(x=factor(1),fill=mpg$class))+
   coord_polar(theta="y") #把y軸方向扭曲,柱子變成彎的
- 
+
 
 #玫瑰花圖
 ggplot(mpg, aes(x = factor(mpg$class),fill=mpg$class)) +
